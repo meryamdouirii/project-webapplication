0026923b5ebb0eafd5d2cef94ac4f316ac131693
meryam <douirimeryam14@gmail.com>
2025-02-12 12:16:42 +0100

    homepage
    debug

--- app/views/header.php
+++ app/views/header.php
@@ -1,6 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="stylesheets/style.css" rel="stylesheet">
+</head>
+<body></body>
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        <a class="navbar-brand" href="#">Haarlem Festival</a>
+        <a class="navbar-brand" href="/">Haarlem Festival</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -9,6 +19,9 @@
                 <li class="nav-item"><a class="nav-link" href="#">Yummy</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Dance</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Tickets</a></li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="button" href="/login">Log In</a></li>
             </ul>
         </div>
     </div>
