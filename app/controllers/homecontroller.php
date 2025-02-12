bfe15b7513af3dff780cc3f193845854f64e74c1
meryam <douirimeryam14@gmail.com>
2025-02-12 12:09:15 +0100

    .

--- app/controllers/homecontroller.php
+++ app/controllers/homecontroller.php
@@ -8,16 +8,10 @@ class HomeController
     public function index() {
         require("../views/index.php");
     }
-    public function musicIcon() {
-        require("../src/images-logos/music-icon.png");
+    public function login(){
+        require("../views/login.php");
     }
-    public function locationIcon() {
-        require("../src/images-logos/location-icon.png");
-    }
-    public function calendarIcon() {
-        require("../src/images-logos/calendar-icon.png");
-    }
-    
+   
 
 }
 
--- app/views/index.php
+++ app/views/index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="nl">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/style.css" rel="stylesheet">
+    <title>Home</title>
 </head>
-
-
-<body>
-<?php include 'header.php'; ?>
+<?php include __DIR__ . '/header.php'; ?>
 <main>
     <section class="hero">
         <div class="hero-overlay">
@@ -27,22 +17,22 @@
         <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-5">
-                <div class="card">
+                <a href="#" class="card">
                     <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?w=500" alt="Food">
                     <div class="card-body">
                         <h5 class="card-title">Yummy</h5>
                         <p>Savor delicious cuisines from around the world.</p>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-md-5">
-                <div class="card">
+                <a href="#" class="card">
                     <img src="https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?w=500" alt="Dance">
                     <div class="card-body">
                         <h5 class="card-title">Dance</h5>
                         <p>Dance the night away with amazing DJs.</p>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
         </div>
@@ -87,7 +77,7 @@
 
 
 </main>
-<?php include 'footer.php'; ?>
+<?php include __DIR__ . '/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
