c05ccba9a1f4d9f8fc2382c9738aa9d617e20c73
Romy <701224@student.inholland.nl>
2025-02-18 13:10:13 +0100

    added recaptcha & error handling

--- app/controllers/homecontroller.php
+++ app/controllers/homecontroller.php
@@ -33,7 +33,7 @@ class HomeController
                 $_SESSION['user'] = $user;
                 $this->index();
             } else {
-                require __DIR__ . '/../views/user/login.php';
+                return $this->showError("Invalid email or password!", $_POST);
             }
         }
 
@@ -45,23 +45,28 @@ class HomeController
             require("../views/user/register.php");
         }
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $recaptcha_secret = "6LdJ6NoqAAAAAG1yuZ0FjAYrk0W0Art77jCau0Ev"; 
+            $recaptcha_response = $_POST['g-recaptcha-response'];
+        
+            // Verify reCAPTCHA response with Google
+            $verify_url = "https://www.google.com/recaptcha/api/siteverify";
+            $response = file_get_contents($verify_url . "?secret={$recaptcha_secret}&response={$recaptcha_response}");
+            $response_data = json_decode($response);
             $email = htmlspecialchars($_POST['email']);
+            if (!$response_data->success){
+                return $this->showError("Please verify that you're not a robot.", $_POST);
+            }
+            if ($this->userService->getByEmail($email) !== null ) {
+                return $this->showError("Email already exists!", $_POST);
+            }
+
+            
             $raw_password = htmlspecialchars($_POST['password']);
             $password = password_hash($raw_password, PASSWORD_DEFAULT);
             $type_of_user = \App\Models\enums\UserType::Customer;
             $first_name = !empty($_POST['first_name']) ? htmlspecialchars($_POST['first_name']) : null;
             $last_name = !empty($_POST['last_name']) ? htmlspecialchars($_POST['last_name']) : null;
             $phone_number = !empty($_POST['phone_number']) ? htmlspecialchars($_POST['phone_number']) : null;
-
-            if ($this->userService->getByEmail($email) !== null) {
-                $error = "This email is already registered.";
-    
-                $formData = $_POST;
-    
-                require __DIR__ . '/../views/user/register.php';
-                exit;
-            }
-
             $user = new \App\Models\User();
             $user->email = $email;
             $user->hashed_password = $password;
@@ -78,6 +83,18 @@ class HomeController
             $this->index();
         }
         
+    }
+
+    private function showError($message, $formData) {
+        $formData = $_POST;
+        require __DIR__ . '/../views/user/register.php';
+        echo "<script>
+            const errorMessageDiv = document.getElementById('error-message');
+            errorMessageDiv.style.display = 'block';
+            errorMessageDiv.textContent = \"$message\";
+        </script>";
+        
+        exit;
     }
 
     public function logout(){
